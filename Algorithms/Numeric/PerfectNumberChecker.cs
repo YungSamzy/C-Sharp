561bc5a72cd99f1fce5ed25ec69d7fbfc6315b12
SamzyDev <61764480+YungSamzy@users.noreply.github.com>
2022-08-14 13:39:34 -0500

    Spelling Error

--- Algorithms/Numeric/PerfectNumberChecker.cs
+++ Algorithms/Numeric/PerfectNumberChecker.cs
@@ -4,7 +4,7 @@ namespace Algorithms.Numeric
 {
     /// <summary>
     ///     In number theory, a perfect number is a positive integer that is equal to the sum of its positive
-    ///     divisors, excluding the number itself.For instance, 6 has divisors 1, 2 and 3 (excluding
+    ///     divisors, excluding the number itself. For instance, 6 has divisors 1, 2 and 3 (excluding
     ///     itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
     /// </summary>
     public static class PerfectNumberChecker
